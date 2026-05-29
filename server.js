03f7ae65873c55f4018b7797bc28fee035efa3f5
Aa852147520 <abc8731721@YAHOO.COM.TW>
2026-05-29 14:23:10 +0800

    Update reply message for activation key request

--- server.js
+++ server.js
@@ -110,8 +110,9 @@ async function handleEvent(event, client) {
   let reply = "";
 
   try {
-    if (text === "我的ID") {
-      reply = `你的 LINE User ID：\n${userId}`;
+    if (text === "開通") {
+      reply = `你的開通密鑰：\n${userId}
+      請聯絡管理員開通:@058gvokk`;
     } else if (text === "說明" || text.toLowerCase() === "help") {
       reply = engine.helpText(vip, isAdmin);
 
